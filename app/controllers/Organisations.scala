5d1c9a18a2e103587f9e9884331cc04356b3f00f
Sietse de Kaper <sietse@lunatech.com>
2013-07-02 14:53:52 +0200

    T4 Implemented organization create form

--- app/controllers/Organisations.scala
+++ app/controllers/Organisations.scala
@@ -1,11 +1,29 @@
 package controllers
 
+import play.api._
 import play.api.mvc._
 import models.{ Organisation, Activity }
 import securesocial.core.SecureSocial
+import play.api.data._
+import play.api.data.Forms._
+import models.Organisation
 import play.api.i18n.Messages
 
 object Organisations extends Controller with SecureSocial {
+
+  val organisationForm = Form(mapping(
+    "id" -> ignored(Option.empty[Long]),
+    "name" -> nonEmptyText,
+    "street1" -> optional(text),
+    "street2" -> optional(text),
+    "city" -> optional(text),
+    "province" -> optional(text),
+    "postCode" -> optional(text),
+    "country" -> nonEmptyText(2, 2),
+    "vatNumber" -> optional(text),
+    "registrationNumber" -> optional(text),
+    "legalEntity" -> default(boolean, false),
+    "active" -> ignored(true))(Organisation.apply)(Organisation.unapply))
 
   /**
    * List page.
@@ -14,6 +32,41 @@ object Organisations extends Controller with SecureSocial {
     val organisations = models.Organisation.findAll
     Ok(views.html.organisation.index(request.user, organisations))
   }
+
+  /**
+   * Create page.
+   */
+  def add = SecuredAction { implicit request ⇒
+    Ok(views.html.organisation.form(request.user, None, organisationForm))
+  }
+
+  /**
+   * Edit page.
+   * @param id The id of the organisation to edit.
+   */
+  def edit(id: Long) = TODO
+
+  /**
+   * Create form submits to this action.
+   */
+  def create = SecuredAction { implicit request ⇒
+    organisationForm.bindFromRequest.fold(
+      formWithErrors ⇒
+        BadRequest(views.html.organisation.form(request.user, None, formWithErrors))
+          .flashing("error" -> Messages("organisation.errors")),
+      organisation ⇒ {
+        val org = organisation.save
+        Logger.info(org.toString)
+        Redirect(routes.Organisations.index()).flashing("success" -> Messages("organisation.created", org.name))
+      })
+  }
+
+  /**
+   * Edit form submits to this action.
+   * @param id
+   * @return
+   */
+  def update(id: Long) = TODO
 
   /**
    * Deletes an organisation.
@@ -25,6 +78,7 @@ object Organisations extends Controller with SecureSocial {
       val message = Messages("success.delete", Messages("models.Organisation"), organisation.name)
       Redirect(routes.Organisations.index).flashing("success" -> message)
     }.getOrElse(NotFound)
+  }
 
   /**
    * Details page.
--- app/models/Organisation.scala
+++ app/models/Organisation.scala
@@ -4,13 +4,30 @@ import play.api.db.slick.Config.driver.simple._
 import play.api.db.slick.DB.withSession
 import play.api.Play.current
 import scala.slick.lifted.Query
+import play.api.db.slick.DB
 
 /**
  * An organisation, usually a company, such as a Happy Melly legal entity.
  */
 case class Organisation(id: Option[Long], name: String, street1: Option[String], street2: Option[String],
   city: Option[String], province: Option[String], postCode: Option[String], countryCode: String,
-  vatNumber: Option[String], registrationNumber: Option[String], legalEntity: Boolean = false, active: Boolean = true)
+  vatNumber: Option[String], registrationNumber: Option[String], legalEntity: Boolean = false, active: Boolean = true) {
+
+  /**
+   * Inserts or updates this organisation into the database.
+   * @return The Organisation as it is saved (with the id added if this was an insert)
+   */
+  def save: Organisation = DB.withSession { implicit session ⇒
+    if (id.isDefined) { // Update
+      Query(Organisations).filter(_.id === id).update(this)
+      this
+    } else { // Insert
+      val id = Organisations.forInsert.insert(this)
+      this.copy(id = Some(id))
+    }
+  }
+
+}
 
 object Organisation {
 
@@ -51,7 +68,6 @@ object Organisations extends Table[Organisation]("ORGANISATION") {
   def province = column[Option[String]]("PROVINCE")
   def postCode = column[Option[String]]("POST_CODE")
 
-  // TODO Change to a list of country codes (i.e. a foreign key)
   def countryCode = column[String]("COUNTRY_CODE")
   def vatNumber = column[Option[String]]("VAT_NUMBER")
   def registrationNumber = column[Option[String]]("REGISTRATION_NUMBER")
