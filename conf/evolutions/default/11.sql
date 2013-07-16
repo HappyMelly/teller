d709fcaa50235ec7742d55d0ed79477d6a4f5379
Peter Hilton <pedro+git@lunatech.com>
2013-07-16 17:01:16 +0200

    Change development database to MySQL

--- conf/evolutions/default/11.sql
+++ conf/evolutions/default/11.sql
@@ -1,6 +1,12 @@
 # --- !Ups
 
-alter table ORGANISATION add column UPDATED TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+-- Repair empty values for CREATED.
+update ORGANISATION set CREATED = CURRENT_TIMESTAMP where CREATED = '0000-00-00 00:00:00';
+
+alter table ORGANISATION add column UPDATED TIMESTAMP;
+update ORGANISATION set UPDATED = CREATED;
+alter table ORGANISATION modify column UPDATED TIMESTAMP NOT NULL;
+
 alter table ORGANISATION add column UPDATED_BY VARCHAR(254) NOT NULL DEFAULT 'Peter Hilton';
 
 # --- !Downs
