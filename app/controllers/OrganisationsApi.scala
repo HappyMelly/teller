ef1142de90b521d95de567b7e15823f1bc5c7ad2
Peter Hilton <pedro+git@lunatech.com>
2013-09-02 16:28:28 +0200

    T61 Organisation details API

--- app/controllers/OrganisationsApi.scala
+++ app/controllers/OrganisationsApi.scala
@@ -2,23 +2,53 @@ package controllers
 
 import play.api.mvc.Controller
 import play.api.libs.json._
-import models.Organisation
+import models.{ Address, Person, Organisation }
 
 /**
  * Organisations API.
  */
 object OrganisationsApi extends Controller with ApiAuthentication {
 
-  implicit val organisationListWrites = new Writes[Organisation] {
+  implicit val organisationWrites = new Writes[Organisation] {
     def writes(organisation: Organisation): JsValue = {
       Json.obj(
-        // "href" -> organisation.id.map(organisationId ⇒ routes.OrganisationsApi.licensee(organisationId).url),
+        "href" -> organisation.id.map(organisationId ⇒ routes.OrganisationsApi.organisation(organisationId).url),
         "name" -> organisation.name,
         "city" -> organisation.city,
         "country" -> organisation.countryCode)
     }
   }
 
+  import PeopleApi.personSummaryWrites
+
+  val organisationDetailsWrites = new Writes[Organisation] {
+    def writes(organisation: Organisation): JsValue = {
+      val address = Address(None, organisation.street1, organisation.street2, organisation.city, organisation.province,
+        organisation.postCode, organisation.countryCode)
+      import PeopleApi.addressWrites
+
+      Json.obj(
+        "name" -> organisation.name,
+        "address" -> Json.toJson(address),
+        "vat_number" -> organisation.vatNumber,
+        "registration_number" -> organisation.registrationNumber,
+        "legal_entity" -> organisation.legalEntity,
+        "members" -> organisation.members)
+    }
+  }
+
+  /**
+   * Organisation details API.
+   */
+  def organisation(id: Long) = TokenSecuredAction { implicit request ⇒
+    Organisation.find(id).map { organisation ⇒
+      Ok(Json.toJson(organisation)(organisationDetailsWrites))
+    }.getOrElse(NotFound("Unknown organization"))
+  }
+
+  /**
+   * Organisation list API.
+   */
   def organisations = TokenSecuredAction { implicit request ⇒
     Ok(Json.toJson(Organisation.findLegalEntities))
   }
