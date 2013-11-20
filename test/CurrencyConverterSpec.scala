299e3b69488fbc3b58cc7876e5230eba13735973
Sietse de Kaper <sietse@lunatech.com>
2013-11-20 22:34:20 +0100

    T92 Implement CurrencyConverter

---
+++ test/CurrencyConverterSpec.scala
@@ -0,0 +1,69 @@
+/*
+ * Happy Melly Teller
+ * Copyright (C) 2013, Happy Melly http://www.happymelly.com
+ *
+ * This file is part of the Happy Melly Teller.
+ *
+ * Happy Melly Teller is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Happy Melly Teller is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Happy Melly Teller.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * If you have questions concerning this license or the applicable additional terms, you may contact
+ * by email Sergey Kotlov, sergey.kotlov@happymelly.com or
+ * in writing Happy Melly One, Handelsplein 37, Rotterdam, The Netherlands, 3071 PR
+ */
+
+import models.ExchangeRate
+import org.joda.money.{Money, CurrencyUnit}
+import org.joda.money.CurrencyUnit._
+import org.joda.time.DateTime
+import org.specs2.mutable.Specification
+import scala.concurrent.Future
+import scala.Some
+import services.{CurrencyConverter, ExchangeRateProvider}
+import play.api.test.Helpers._
+
+class CurrencyConverterSpec extends Specification {
+
+  val eurUsd = ExchangeRate(EUR, USD, BigDecimal("1.35098"), DateTime.now())
+  val oneEuro = Money.of(EUR, 1)
+
+  val someProvider = new ExchangeRateProvider {
+    def apply(base: CurrencyUnit, counter: CurrencyUnit): Future[Option[ExchangeRate]] = Future.successful(Some(eurUsd))
+  }
+
+  val noneProvider = new ExchangeRateProvider {
+    def apply(base: CurrencyUnit, counter: CurrencyUnit): Future[Option[ExchangeRate]] = Future.successful(None)
+  }
+
+  val someFirst = someProvider :: noneProvider :: Nil
+  val noneFirst = noneProvider :: someProvider :: Nil
+  val noneOnly = noneProvider :: Nil
+
+  "The currency converter" should {
+    "pick the first provider that can provide an exchange rate" in {
+      await(CurrencyConverter.findRate(EUR, USD, someFirst)) should beSome(eurUsd)
+      await(CurrencyConverter.findRate(EUR, USD, noneFirst)) should beSome(eurUsd)
+      await(CurrencyConverter.findRate(EUR, USD, noneOnly)) should beNone
+    }
+
+    "convert between currencies it has a provider for" in {
+      await(CurrencyConverter.convert(oneEuro, USD, someFirst)).isSuccess must beTrue
+      await(CurrencyConverter.convert(oneEuro, USD, someFirst)).get must beEqualTo(eurUsd(oneEuro))
+    }
+
+    "fail conversions for which there is no exchange rate" in {
+      await(CurrencyConverter.convert(oneEuro, USD, noneOnly)).isSuccess must beFalse
+    }
+  }
+
+}
