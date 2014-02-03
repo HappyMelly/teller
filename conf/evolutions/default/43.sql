77a9bb37d95ae2543de917635f95226b552d8e34
Peter Hilton <pedro+git@lunatech.com>
2014-02-03 17:13:17 +0100

    T107 Fix git merge fail

--- conf/evolutions/default/43.sql
+++ conf/evolutions/default/43.sql
@@ -6,4 +6,6 @@ alter table BOOKING_ENTRY modify column BRAND_ID bigint;
 
 # --- !Downs
 
+delete from ACTIVITY where PREDICATE = 'balance';
+delete from BOOKING_ENTRY where BRAND_ID is null;
 alter table BOOKING_ENTRY modify column BRAND_ID bigint not null;
