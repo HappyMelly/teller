52c666dc1a0b4fb3296de9abfeea10059d45f66e
Sergey Kotlov <skotlov@gmail.com>
2014-03-11 21:42:35 +0400

    Play Evolution fix

--- conf/evolutions/default/51.sql
+++ conf/evolutions/default/51.sql
@@ -8,14 +8,15 @@ create table EVENT_INVOICE (
   NUMBER VARCHAR(254)
 );
 
-alter table EVENT_INVOICE add constraint EVENT_INVOICE_FK foreign key(EVENT_ID) references EVENT(ID) on update NO ACTION on delete NO ACTION;
-alter table EVENT_INVOICE add constraint INVOICE_TO_FK foreign key(INVOICE_TO) references ORGANISATIONS(ID) on update NO ACTION on delete NO ACTION;
 # it's just too difficult to update all records. No reason to waste my time because they exist only on a test server
-alter table EVENT_FACILITATOR drop constraint EVENT_FK
+alter table EVENT_FACILITATOR drop foreign key EVENT_FK;
+truncate table EVENT_FACILITATOR;
 truncate table EVENT;
+
+alter table EVENT_INVOICE add constraint EVENT_INVOICE_FK foreign key(EVENT_ID) references EVENT(ID) on update NO ACTION on delete NO ACTION;
 alter table EVENT_FACILITATOR add constraint EVENT_FK foreign key(EVENT_ID) references EVENT(ID) on update NO ACTION on delete NO ACTION;
 
 # --- !Downs
 
 alter table EVENT_INVOICE drop foreign key EVENT_INVOICE_FK;
-alter table EVENT_INVOICE drop foreign key INVOICE_TO_FK;
+drop table EVENT_INVOICE;
