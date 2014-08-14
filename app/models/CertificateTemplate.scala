aacd65f74c18363f8332baeef7aad0d8353fb5c4
Sergey Kotlov <skotlov@gmail.com>
2014-08-14 14:29:13 +0400

    Fixes after code review

--- app/models/CertificateTemplate.scala
+++ app/models/CertificateTemplate.scala
@@ -25,12 +25,9 @@
 package models
 
 import models.database.CertificateTemplates
-import org.joda.time.DateTime
 import play.api.db.slick.Config.driver.simple._
 import play.api.db.slick.DB
 import play.api.Play.current
-import play.api.libs.Crypto
-import scala.util.Random
 
 /**
  * A thing such as a book, a game or a piece of software
@@ -50,45 +47,13 @@ case class CertificateTemplate(
     val id = CertificateTemplates.forInsert.insert(this)
     this.copy(id = Some(id))
   }
-  //
-  //  def delete(): Unit = Product.delete(this.id.get)
-  //
-  //  def update: Product = DB.withSession { implicit session: Session ⇒
-  //    val updateTuple = (title, subtitle, url, description, callToActionUrl, callToActionText, picture, category, parentId, updated, updatedBy)
-  //    val updateQuery = Products.filter(_.id === this.id).map(_.forUpdate)
-  //    updateQuery.update(updateTuple)
-  //    this
-  //  }
+
   def delete(): Unit = DB.withSession { implicit session: Session ⇒
     CertificateTemplates.where(_.id === this.id).mutate(_.delete())
   }
 }
 
 object CertificateTemplate {
-
-  //  def cacheId(id: Long): String = "products." + id.toString
-  //
-  //  def generateImageName(filename: String): String = "products/" + Crypto.sign("%s-%s".format(filename, Random.nextInt())) + ".png"
-  //
-  //  def exists(title: String): Boolean = DB.withSession { implicit session: Session ⇒
-  //    Query(Query(Products).filter(_.title === title).exists).first
-  //  }
-  //  /**
-  //   * Returns true if and only if there is a product with the given title.
-  //   */
-  //  def exists(title: String, id: Long): Boolean = DB.withSession { implicit session: Session ⇒
-  //    Query(Query(Products).filter(_.title === title).filter(_.id =!= id).exists).first
-  //  }
-  //
-  //  /** Finds a product by ID **/
-  //  def find(id: Long) = DB.withSession { implicit session: Session ⇒
-  //    Query(Products).filter(_.id === id).firstOption
-  //  }
-  //
-  //  def findDerivatives(parentId: Long): List[Product] = DB.withSession { implicit session: Session ⇒
-  //    Query(Products).filter(_.parentId === parentId).list
-  //  }
-  //
 
   /**
    * Find all brand-related certificate files
@@ -109,10 +74,5 @@ object CertificateTemplate {
   def find(id: Long): Option[CertificateTemplate] = DB.withSession { implicit session: Session ⇒
     Query(CertificateTemplates).filter(_.id === id).firstOption
   }
-
-  //
-  //  def delete(id: Long): Unit = DB.withSession { implicit session: Session ⇒
-  //    Products.where(_.id === id).mutate(_.delete())
-  //  }
 
 }
