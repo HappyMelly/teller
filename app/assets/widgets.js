d6b8b788d66a1cc3491d9e6a4052911709700db1
Eremin slawa <webvitalik@gmail.com>
2016-01-08 16:52:04 +0300

    update widgets

--- app/assets/widgets.js
+++ app/assets/widgets.js
@@ -101,6 +101,176 @@
 })(jQuery, App);
 
 
+/**
+ *  Upload photo widget
+ */
+(function ($, App) {
+    'use strict';
+
+    function UploadPhotoWidget(options){
+        var self = this;
+
+        self.$el = $(options.selector);
+
+        self.defaults = {
+            urlUpdate: '',
+            urlContent: '',
+            uploadedPhoto: null
+        }
+
+        self.locals = {
+            selector: options.selector,
+            $el: $(options.selector),
+            $modalContent: self.$el.find('#uploadPhotoContent'),
+            $modal: self.$el.find('#uploadPhotoDialog'),
+            $uploadSaveBtn: self.$el.find('#uploadBtnSave'),
+            $uploadCustomPhoto: self.$el.find('#uploadCustomPhoto')
+        };
+
+        $.extend(this, self.defaults, options);
+
+        self.assignEvents();
+        self.setupCustomPhotoActions();
+    }
+
+    UploadPhotoWidget.prototype.assignEvents = function(){
+        var self = this;
+
+        self.locals.$el
+            .on('click', '.js-choose-link', function(e){
+                self.showSelectPhotoForm();
+                self.locals.$modal.modal('show');
+
+                e.preventDefault();
+            })
+            .on('click', '.b-photoupload__item', function (e) {
+                var $this = $(this);
+
+                self.switchActivePhoto($this);
+                e.preventDefault();
+            })
+            .on('click', '#uploadBtnSave', function (e) {
+                self.updatePhoto();
+                e.preventDefault();
+            })
+            .on('click', '#uploadBtnDelete', function (e) {
+                self.deletePhoto();
+                e.preventDefault();
+            })
+            .on('fileuploadadd', '#photoUpload', function (e, data) {
+                self.uploadedPhoto = data;
+                self.locals.$uploadCustomPhoto
+                    .attr('src', URL.createObjectURL(data.files[0]))
+                    .addClass('photo');
+            })
 
 
+        App.events.sub('hmtShowSelectPhotoForm', function(arr){
+            self.showSelectPhotoForm();
 
+            setTimeout(function(){
+                $(arr[0]).modal('show');
+            }, 200);
+            e.preventDefault();
+        });
+    };
+
+    UploadPhotoWidget.prototype.showSelectPhotoForm = function(){
+        var self = this;
+
+        $.get( self.urlContent, function(data) {
+            self.locals.$modalContent.html(data);
+
+            self.setupCustomPhotoActions();
+        });
+    };
+
+    UploadPhotoWidget.prototype.switchActivePhoto = function($object){
+        var self = this;
+
+        if (!$object.length) return;
+
+        self.locals.$el
+            .find('.b-photoupload__item').removeClass('type_active');
+        $object.addClass('type_active');
+    }
+
+
+    UploadPhotoWidget.prototype.setupCustomPhotoActions = function(){
+       var self = this;
+
+
+        $('#photoUpload').fileupload({
+            dataType: 'json',
+            disableImageResize: false,
+            imageMaxWidth: 300,
+            imageMaxHeight: 300,
+            imageCrop: false,
+            autoUpload: false,
+            replaceFileInput: false,
+            done: function (e, data) {
+                self.locals.$uploadCustomPhoto
+                    .attr('src', data.result.link);
+
+                self.switchActivePhoto(
+                    self.locals.$uploadCustomPhoto.closest('.b-photoupload__item')
+                );
+            }
+        })
+
+        initializeFileUploadField();
+
+    }
+
+    UploadPhotoWidget.prototype.updatePhoto = function(){
+        var self = this,
+            $currentItem = self.locals.$modalContent.find('.type-active'),
+            type = $currentItem.attr('id'),
+            src = $currentItem.find('b-photoupload__img').attr('src');
+
+        if (type == "custom" && self.uploadedPhoto != null) {
+            self.locals.$uploadSaveBtn.text('Uploading...');
+            self.uploadedPhoto.submit();
+        }
+
+        $.post(
+            self.urlUpdate,
+            { type: type },
+            null,
+            "json"
+        ).done( self.updateSuccess)
+    };
+
+    UploadPhotoWidget.prototype.updateSuccess = function(data){
+        self.local.$modal.modal('hide');
+
+        $('.b-avatar__real .b-photoupload__img')
+            .first()
+            .attr('src', src);
+
+        $('.b-avatar')
+            .addClass('b-avatar_stat_real');
+
+        App.events.pub('hmtReloadCompletionWidgethmt');
+    }
+
+    UploadPhotoWidget.prototype.deletePhoto = function(){
+        var self = this;
+
+        $.ajax({
+            type: "DELETE",
+            url: $(this).data('href'),
+            dataType: "json"
+        }).done(function() {
+            $('.b-avatar')
+                .removeClass('b-avatar_stat_real');
+
+            App.events.pub('hmtReloadCompletionWidget');
+        });
+
+        return false;
+    };
+
+    App.widgets.UploadPhotoWidget = UploadPhotoWidget;
+
+})(jQuery, App);
