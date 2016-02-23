2377b4b9e4336959222f3e59b3750df9f21a6bcf
Eremin slawa <webvitalik@gmail.com>
2016-02-23 18:33:13 +0300

    525 - create separate Form validation

--- app/assets/person/manage-accounts.js
+++ app/assets/person/manage-accounts.js
@@ -4,8 +4,18 @@ $(function(){
         new App.widgets.PersonSocialConnect('.js-social-connect');
     }
 
-    var $emailConnect = $('.js-email-connect');
-    if ($emailConnect.length){
-        new App.widgets.PersonEmailConnect('.js-email-connect');
-    }
+    new App.widgets.EmailConnectDlg('#dlg-email-connect', {
+        url: jsRoutes.controllers.UserAccounts.handleNewPassword().url,
+        success: function(){
+           $('.js-email-connect').addClass('show_connected');
+        }
+    });
+
+    new App.widgets.EmailConnectDlg('#dlg-change-password', {
+        url: jsRoutes.controllers.UserAccounts.changePassword().url
+    });
+
+    new App.widgets.EmailConnectDlg('#dlg-change-email', {
+        url: jsRoutes.controllers.UserAccounts.changeEmail().url,
+    });
 });
--- app/assets/person/widgets/email-connect.js
+++ app/assets/person/widgets/email-connect.js
@@ -1,184 +1,67 @@
+
 /**
- *  Email connect
+ *  Dialog
  */
 (function ($, App) {
     'use strict';
 
-    function PersonEmailConnect(selector, options){
+    function EmailConnectDlg(selector, options){
         var self = this;
 
         self.$root = $(selector);
-        self.options = $.extend({}, options, self.$root.data());
         self.locals = {
-            $currentEmail: self.$root.find('[data-email-current]'),
-            $createEmailForm: self.$root.find('[data-create-email]'),
-            $changePassForm: self.$root.find('[data-change-pass]'),
-            $changeEmailForm: self.$root.find('[data-change-email]')
+            $form: self.$root.find('form'),
+            $inputs: self.$root.find('form input')
         };
+        self.options = $.extend({}, options, self.$root.data());
+        self.validation = new App.widgets.FormValidation(self.locals.$inputs);
 
         self.assignEvents();
     }
 
-    PersonEmailConnect.prototype.assignEvents = function(){
+    EmailConnectDlg.prototype.assignEvents = function(){
         var self = this;
 
-        self.locals.$createEmailForm.on('submit', '', function (e) {
-            var $this = $(this);
+        self.locals.$form.on('submit', function (e) {
+            var $inputs = self.locals.$inputs;
             e.preventDefault();
 
-            if (!self.isFormValid($this)) return;
-            self.createEmail($this);
+            if (!self.validation.isValidInputs($inputs)) return;
+            self.submitForm();
         });
 
-        self.locals.$changePassForm.on('submit', '', function (e) {
-            var $this = $(this);
-            e.preventDefault();
-
-            if (!self.isFormValid($this)) return;
-            self.changePassword($this);
-        });
-
-        self.locals.$changeEmailForm.on('submit', '', function (e) {
-            var $this = $(this);
-            e.preventDefault();
-
-            if (!self.isFormValid($this)) return;
-            self.changeEmail($this);
-        });
-
-        self.$root
-            .on('input', '.modal input', function(e){
-                var $this = $(this);
-                self.removeError($this);
-            })
-            .on('hidden.bs.modal', function(e){
-                var $modal = $(e.target);
-
-                self.clearForm($modal.find('form'));
-            })
+        self.$root.on('hide.bs.modal', function(e){
+            self.validation.clearForm();
+            self.validation.removeErrors();
+        })
     };
 
-    PersonEmailConnect.prototype.isFormValid = function($form){
+    EmailConnectDlg.prototype.submitForm = function(){
         var self = this,
-            $inputs = $form.find('input'),
-            error = 0;
+            ajaxData = {};
 
-        $inputs.each(function(index, el){
+        self.locals.$inputs.each(function(index, el){
             var $el = $(el),
-                value = $.trim($el.val());
+                name = $el.attr('name');
 
-            if ($el.hasClass('type-email') && !self.isValidEmail(value)){
-                self.setError($el, 'Email is not valid');
-                error += 1;
-            }
-
-            if ($el.hasClass('is-the-same') && !self.isSamePass($form, $el)){
-                self.setError($el, 'Password should be the same');
-                error += 1;
-            }
-
-            if (!value) {
-                self.setError($el, 'Empty');
-                error += 1;
-            }
+            name && (ajaxData[name] = $el.val())
         });
 
-        return !error;
-    };
+        $.post(self.options.url, ajaxData, function(){
+            self.$modal.modal('hide');
+            self.validation.clearForm();
 
-    PersonEmailConnect.prototype.createEmail = function($form){
-        var self = this,
-            $modal = $form.closest('.modal'),
-            $newEmail = $modal.find('input[name="email"]').val(),
-            url = jsRoutes.controllers.UserAccounts.handleNewPassword().url;
+            self.options.success && self.options.success();
+        }).fail(function(data){
+            if (!data.errors) return;
+            self.validation.setErrors(data.errors);
 
-        $.post(url, {
-            email: $form.find('input[name="email"]').val(),
-            password: $form.find('input[name="password"]').val()
-        }, function(){
-            $modal.modal('hide');
-            self.$root.addClass('show_connected');
-            self.locals.$currentEmail.text($newEmail);
-        }).fail(function(){
-
+            self.options.fail && self.options.fail();
         })
     };
 
-    PersonEmailConnect.prototype.changePassword = function($form){
-        var self = this,
-            $modal = $form.closest('.modal'),
-            url = jsRoutes.controllers.UserAccounts.changePassword().url;
-
-        $.post(url, {
-            oldpassword: $form.find('input[name="old-password"]').val(),
-            password: $form.find('input[name="password"]').val()
-        }, function(){
-            $modal.modal('hide');
-            self.clearForm($form);
-        }).fail(function(){
-
-        })
-    };
-
-    PersonEmailConnect.prototype.changeEmail = function($form){
-        var self = this,
-            $modal = $form.closest('.modal'),
-            $newEmail = $modal.find('input[name="new-email"]').val(),
-            url = jsRoutes.controllers.UserAccounts.changeEmail().url;
-
-        $.post(url, {
-            oldemail: $form.find('input[name="old-email"]').val(),
-            email: $form.find('input[name="email"]').val(),
-            password: $form.find('input[name="hmt-password"]').val()
-        }, function(){
-            $modal.modal('hide');
-            self.locals.$currentEmail.text($newEmail);
-            self.clearForm($form);
-        }).fail(function(){
-
-        })
-    };
-
-    PersonEmailConnect.prototype.isValidEmail = function(email) {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
-    };
-
-    PersonEmailConnect.prototype.isSamePass = function($form, $el) {
-        var $originalPass = $form.find('input[name="password"]');
-
-        return $originalPass.val() == $el.val();
-    };
-
-    PersonEmailConnect.prototype.removeError = function($el){
-        var $parent = $el.closest('.email-dlg__i');
-
-        $parent.removeClass('state-error');
-    };
-
-    PersonEmailConnect.prototype.setError = function($el, text){
-        var $parent = $el.closest('.email-dlg__i'),
-            $error = $parent.find('.item-error');
-
-        $parent.addClass('state-error');
-
-        if (!$error.length){
-            $error = $('<div />', {
-                "class": "item-error"
-            }).appendTo($parent);
-        }
-        $error.text(text);
-    };
-
-    PersonEmailConnect.prototype.clearForm = function($form){
-        var $inputs = $form.find('input');
-
-        $inputs.each(function(index, el){
-            var $el = $(el);
-            if (!$el.attr("disabled"))  $el.val('');
-        })
-    };
-
-    App.widgets.PersonEmailConnect = PersonEmailConnect;
+    App.widgets.EmailConnectDlg = EmailConnectDlg;
 
 })(jQuery, App);
+
+
