c9e9f14b258e21286150388e05184f7d12bead27
Eremin slawa <webvitalik@gmail.com>
2016-03-28 12:59:43 +0300

    fix url on the event detail page

--- public/js/event/details-page.js
+++ public/js/event/details-page.js
@@ -50,7 +50,7 @@ $(document).ready( function() {
     }
 
     // Datatables
-    var url = jsRoutes.controllers.event.Attendees.listByEvent($("#eventId").val()).url;
+    var url = jsRoutes.controllers.cm.event.Attendees.listByEvent($("#eventId").val()).url;
     var participantTable = $('#participants').dataTable({
         "sDom": '<"toolbar">rtip',
         "iDisplayLength": 25,
