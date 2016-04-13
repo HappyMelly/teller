2657c7b983cd16d286bffea927c7d63b4f5cc0bc
Eremin slawa <webvitalik@gmail.com>
2016-04-13 18:49:29 +0300

    move payment widgets to the es6

--- frontend/js/payment/payment-widgets/_card-form.js
+++ frontend/js/payment/payment-widgets/_card-form.js
@@ -1,13 +1,13 @@
-(function ($, App) {
-    'use strict';
+'use strict';
 
-    /**
-     * Form for payment with fee
-     * @param selector
-     * @constructor
-     */
-    function CardFeeForm(selector){
-        var self = this;
+/**
+ * Form for updating card information
+ * @param selector
+ * @constructor
+ */
+export default class Widget {
+    constructor(selector) {
+        const self = this;
 
         self.$root = $(selector);
         self.locals = self._getDom();
@@ -17,21 +17,19 @@
             .done(function(){
                 Stripe.setPublishableKey(self.apikey);
                 self._init();
-
-                self._updateAmount(self.locals.$inputFee);
                 self._assignEvents();
             })
     }
 
-    CardFeeForm.prototype._getStripeScript = function(){
+    _getStripeScript(){
         return $.ajax({
             url: 'https://js.stripe.com/v2/',
             dataType: "script"
         });
     };
 
-    CardFeeForm.prototype._getDom = function(){
-        var $root = this.$root;
+    _getDom(){
+        const $root = this.$root;
 
         return {
             $inputNumber: $root.find('[data-card-number]'),
@@ -39,15 +37,11 @@
             $inputMonth: $root.find('[data-card-month]'),
             $inputYear: $root.find('[data-card-year]'),
             $inputCVC: $root.find('[data-card-cvc]'),
-            $submit: $root.find('[data-payment-submit]'),
-            $inputFee: $root.find('[data-payment-fee]'),
-            $taxPlace: $root.find('[data-payment-tax]'),
-            $amountPlace: $root.find('[data-payment-amount]'),
-            $payPlace: $root.find('[data-payment-price]')
+            $submit: $root.find('[data-card-submit]')
         }
     };
 
-    CardFeeForm.prototype._init = function(){
+    _init(){
         if (!$.fn.payment){
             console.log('There is no payment plugin on this page');
             return;
@@ -55,18 +49,13 @@
 
         this.locals.$inputNumber.payment('formatCardNumber');
         this.locals.$inputCVC.payment('formatCardCVC');
+
     };
 
-    CardFeeForm.prototype._assignEvents = function(){
-        var self = this;
+    _assignEvents(){
+        const self = this;
 
         self.$root
-            .on('change paste keyup', '[data-payment-fee]', function (e) {
-                var $this = $(this);
-
-                self._removeError($this);
-                self._updateAmount($this);
-            })
             .on('keyup', 'input', function(){
                 self._removeError($(this));
             })
@@ -77,47 +66,33 @@
             .on('submit', self._onSubmitHandler.bind(self));
     };
 
-    CardFeeForm.prototype._updateAmount = function($el){
-        var locals = this.locals,
-            amount = $el.val() < 1? 0.00: parseInt($el.val()),
-            taxPercent = parseFloat($el.data('tax')),
-            tax, amountWithTax;
-
-        tax = (amount * taxPercent) / 100;
-        amountWithTax = amount + tax;
-
-        locals.$amountPlace.text(amount);
-        locals.$taxPlace.text(tax);
-        locals.$payPlace.text(amountWithTax);
-    };
-
-    CardFeeForm.prototype._setError = function($el){
+    _setError($el){
         $el.parent().addClass('has-error');
     };
 
-    CardFeeForm.prototype._removeError = function($el){
+    _removeError($el){
         $el.parent().removeClass('has-error');
     };
 
-    CardFeeForm.prototype._disabledForm = function(){
+    _disabledForm(){
         this.locals.$submit.prop('disabled', true);
         $("body").css("cursor", "progress");
     };
 
-    CardFeeForm.prototype._enabledForm = function(){
+    _enabledForm(){
         this.locals.$submit.prop('disabled', false);
         $("body").css("cursor", "default");
     };
 
-    CardFeeForm.prototype._addTokenInput = function(token){
-        var $root = this.$root,
+    _addTokenInput(token){
+        let $root = this.$root,
             template = '<input type="hidden" value="' +  token +'" name="token" />';
 
         $root.find('input[name="token"]').remove()
         $root.append(template);
     };
 
-    CardFeeForm.prototype._onSubmitHandler = function(e){
+    _onSubmitHandler(e){
         var self = this;
         e.preventDefault();
 
@@ -127,14 +102,13 @@
         Stripe.card.createToken(self.$root, self._stripeHandler.bind(self));
     };
 
-    CardFeeForm.prototype._stripeHandler = function (status, response) {
-        var self = this,
-            data, errorMsg;
+    _stripeHandler(status, response) {
+        const self = this;
+        let data, errorMsg;
 
         if (response.error) {
             self._enabledForm();
         } else {
-
             self._addTokenInput(response.id);
 
             data = this.$root.serialize();
@@ -142,18 +116,20 @@
                 .done(function (data) {
                     if (data.hasOwnProperty("redirect")) {
                         window.location = data.redirect;
+                    } else if (data.hasOwnProperty("message")) {
+                        success(data.message);
                     } else {
                         var msg = "Internal error #2001. Your card has been charged. ";
                         msg += "Do not make payment again. Please proceed to your profile directly.";
-                        error(msg, 4500);
+                        error(msg);
                     }
                 })
                 .fail(function (jqXHR, status) {
                     if (status == "error") {
                         errorMsg = JSON.parse(jqXHR.responseText);
-                        error(errorMsg.message, 4500);
+                        error(errorMsg.message);
                     } else {
-                        error("Internal error #2000. Your card has not been charged. Please try again.", 4500);
+                        error("Internal error #2000. Your card has not been charged. Please try again.");
                     }
                 })
                 .complete(function () {
@@ -162,23 +138,23 @@
         }
     };
 
-    CardFeeForm.prototype._sendFormData = function(dataForm){
+    _sendFormData(dataForm){
         return $.ajax({
             type: "POST",
             url: this.$root.attr("action"),
-            data: dataForm
+            data: dataForm,
+            dataType: "json"
         })
     };
 
-    CardFeeForm.prototype.isValidForm = function(){
-        var self = this,
+    isValidForm(){
+        let self = this,
             valid = true,
             locals = this.locals,
             isValidNumber = $.payment.validateCardNumber(locals.$inputNumber.val()),
             isValidExpiry = $.payment.validateCardExpiry(locals.$inputMonth.val(), locals.$inputYear.val()),
             isValidCVC = $.payment.validateCardCVC(locals.$inputCVC.val()),
-            isValidName = +locals.$inputName.val().length,
-            isValidAmount = (locals.$inputFee.val().length > 0) && (!isNaN(locals.$inputFee.val()));
+            isValidName = +locals.$inputName.val().length;
 
         if (!isValidNumber){
             self._setError(locals.$inputNumber);
@@ -201,14 +177,22 @@
             valid = false;
         }
 
-        if (!isValidAmount){
-            valid = false;
-            this._setError(locals.$inputFee);
-        }
-
         return valid;
-    };
+    }
 
+    // static
+    static plugin(selector, options) {
+        const $elems = $(selector);
+        if (!$elems.length) return;
 
-    App.widgets.CardFeeForm = CardFeeForm;
-})(jQuery, App);
+        return $elems.each(function (index, el) {
+            let $element = $(el);
+            let data = $element.data('widget.scrollto');
+
+            if (!data) {
+                data = new Widget(el, options);
+                $element.data('widget', data);
+            }
+        })
+    }
+}
