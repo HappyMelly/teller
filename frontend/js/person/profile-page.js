82abad04dd86ef4c6c494c58f22445285d8d9326
Sergey Kotlov <skotlov@gmail.com>
2016-04-21 09:15:22 +0300

    Partially connects MailChimp UI with backend

--- frontend/js/person/profile-page.js
+++ frontend/js/person/profile-page.js
@@ -6,14 +6,14 @@ import IntegrationMailchimp from "./person-widgets/_integration";
 $(function(){
     App.events.sub('hmt.tab.shown', () =>{
         IntegrationMailchimp.plugin('.js-mailchimp-integration', {
-            activate: '/integration/activate',
-            deactivate: '/integration/deactivate',
+            activate: jsRoutes.controllers.cm.facilitator.MailChimp.activate().url,
+            deactivate: jsRoutes.controllers.cm.facilitator.MailChimp.deactivate().url,
 
-            getAvailableLists: '/lists/',
+            getAvailableLists: jsRoutes.controllers.cm.facilitator.MailChimp.lists().url,
 
-            createImport: '/imports/create',
+            createImport: jsRoutes.controllers.cm.facilitator.MailChimp.connect().url,
             updateImport: '/imports/update/1',
-            disableImport: '/imports/disable/1'
+            disableImport: jsRoutes.controllers.cm.facilitator.MailChimp.disconnect().url
         })
     })
 
